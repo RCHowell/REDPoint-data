52250fbafb5c1a1f6eb2a927283e2de352eb3d89
R. C. Howell <rhowell2@nd.edu>
2018-01-22 13:21:16 -0500

    Include a join_example with GROUP_CONCAT on tag names when querying routes

--- sqlite/join_examples.sql
+++ sqlite/join_examples.sql
@@ -44,4 +44,27 @@ SELECT
     LEFT JOIN routes ON routes.route_id = tags.route_id
   ) tags
 FROM routes as R
-LIMIT 1
+LIMIT 1;
+
+-- Query a route based upon attributes and tags
+SELECT 
+  r.route_id, r.name, r.length, r.stars, r.type, r.grade,
+  GROUP_CONCAT(t.tag) AS tags
+  FROM routes r
+LEFT JOIN tags t
+  ON t.route_id = r.route_id
+WHERE
+  r.length >= 0.0 AND
+  r.stars >= 0.0 AND
+  r.grade_int >= 0 AND
+  r.type IN ('Trad', 'TradOriginal') AND
+  t.tag IN ('arete', 'steep')
+GROUP BY r.route_id
+LIMIT 10;
+
+SELECT * FROM tags WHERE
+  tag IN ('pumpy', 'juggy');
+
+
+
+
